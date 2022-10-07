d1dd5ceac7796bf48d44721510a91933af290786
Vu1092001 <112168479+Vu1092001@users.noreply.github.com>
2022-10-06 21:52:01 -0700

    update

--- QLyChuyenBayJPA/src/main/java/com/example/QuanLyChuyenBayJPA/controller/controller.java
+++ QLyChuyenBayJPA/src/main/java/com/example/QuanLyChuyenBayJPA/controller/controller.java
@@ -14,38 +14,67 @@ import com.example.QuanLyChuyenBayJPA.entity.NhanVien;
 import com.example.QuanLyChuyenBayJPA.repository.ChuyenBayRepository;
 import com.example.QuanLyChuyenBayJPA.repository.MayBayRepository;
 import com.example.QuanLyChuyenBayJPA.repository.NhanVienRepository;
+import com.example.QuanLyChuyenBayJPA.service.ChuyenBayService;
+import com.example.QuanLyChuyenBayJPA.service.MayBayService;
+import com.example.QuanLyChuyenBayJPA.service.NhanVienService;
 
 @RestController
 public class controller {
+	@Autowired 
+	ChuyenBayService chuyenBayService;
 	@Autowired
-	ChuyenBayRepository chuyenBayRepository; 
+	MayBayService mayBayService;
 	@Autowired
-	MayBayRepository mayBayRepository;
-	@Autowired
-	NhanVienRepository nhanVienRepository;
+	NhanVienService nhanVienService;
 	//////////////////Cho biết các chuyến bay đi Đà Lạt (DAD)./////////////////////////
 	@GetMapping("/bai1")
 	public List<ChuyenBay> bai1() {
-		return chuyenBayRepository.findChuyenBaysDaLat();
+		return chuyenBayService.findChuyenBaysDaLat();
 	}
 	///////////////////Cho biết các loại máy bay có tầm bay lớn hơn 10,000km////////////////////////
 	@GetMapping("/bai2")
 	public List<MayBay> bai2(){
-		return mayBayRepository.findMayBayTamBayLH10000();
+		return mayBayService.findMayBayTamBayLH10000();
 	}
 	////////////////Tìm các nhân viên có lương nhỏ hơn 10,000.///////////////////////////
 	@GetMapping("/bai3")
 	public List<NhanVien> bai3(){
-		return nhanVienRepository.findNVLuongNH10000();
+		return nhanVienService.findNVLuongNH10000();
 	}
 	////////////////Cho biết các chuyến bay có độ dài đường bay nhỏ hơn 10.000km và lớn hơn 8.000km.////////////////
 	@GetMapping("/bai4")
 	public List<ChuyenBay> bai4(){
-		return chuyenBayRepository.findChuyenBaysDoDaiNH10000LH8000();
+		return chuyenBayService.findChuyenBaysDoDaiNH10000LH8000();
 	}
 	//Cho biết các chuyến bay xuất phát từ Sài Gòn (SGN) đi Ban Mê Thuộc (BMV).
 	@GetMapping("/bai5")
 	public List<ChuyenBay> findChuyenBaysfromSGNtoBMV(){
-		return chuyenBayRepository.findChuyenBaysfromSGNtoBMV();
-	} 
+		return chuyenBayService.findChuyenBaysfromSGNtoBMV();
+	}
+	//Có bao nhiêu chuyến bay xuất phát từ Sài Gòn (SGN)
+	@GetMapping("/bai6")
+	public int CountChuyenBaysfromSG() {
+		return chuyenBayService.CountChuyenBaysfromSG();
+	}
+	//Có bao nhiêu loại máy báy Boeing
+	@GetMapping("/bai7")
+	public int CountMayBayLoaiBoeing() {
+		return mayBayService.CountMayBayLoaiBoeing();
+	}
+	//Cho biết tổng số lương phải trả cho các nhân viên.
+	@GetMapping("/bai8")
+	public int TongLuongNVPhaiTra() {
+		return nhanVienService.TongLuongNVPhaiTra();
+	}
+	//Cho biết mã số của các phi công lái máy báy Boeing.
+	@GetMapping("/bai9")
+	public List<String> findNVLaiBoeing(){
+		return nhanVienService.findNVLaiBoeing();
+	}
+	//Cho biết các nhân viên có thể lái máy bay có mã số 747
+	@GetMapping("/bai10")
+	public List<NhanVien> findNVByMaMB(){
+		return nhanVienService.findNVByMaMB();
+	}
+	
 }
